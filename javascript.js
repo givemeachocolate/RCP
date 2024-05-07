61f73a7b3d745bef84cedadcc9870a4e0d190835
givemeachocolate <132573959+givemeachocolate@users.noreply.github.com>
2024-05-07 17:10:34 +0900

    Edit index.html and javascript.js

--- javascript.js
+++ javascript.js
@@ -39,6 +39,7 @@
 
 let a = ['rock', 'scissors', 'paper']
 let c = 0
+let d = 0
 
 function getComputerChoice() {
    b = Math.floor(Math.random()*3)
@@ -47,18 +48,50 @@ function getComputerChoice() {
 
 // alert(getComputerChoice())
 
+// function playRound(playerSelection, computerSelection) {
+//     playerSelection = playerSelection.toLowerCase()
+//     if (playerSelection === computerSelection) {
+//         return "Tie"
+//     }
+//     else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock') ) {
+//     c += 1
+//     return `You Win! ${playerSelection} beats ${computerSelection}`
+//     }
+//     else {
+//     c -= 1
+//     return `You Lose! ${computerSelection} beats ${playerSelection}`
+//     }
+// }
+
 function playRound(playerSelection, computerSelection) {
+    const score = document.createElement("ul")
+    computerSelection = getComputerChoice()
     playerSelection = playerSelection.toLowerCase()
     if (playerSelection === computerSelection) {
+        console.log("Tie")
         return "Tie"
     }
     else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'rock') ) {
     c += 1
-    return `You Win! ${playerSelection} beats ${computerSelection}`
+    // score.textContent = `You Win! ${playerSelection} beats ${computerSelection}`
+    score.textContent = "Your score: " + c
+    div.appendChild(score)
+    // return `You Win! ${playerSelection} beats ${computerSelection}`
+        if (c === 5) {
+            score.textContent = 'You Win!!'
+            div.appendChild(score)
+        }
     }
     else {
-    c -= 1
-    return `You Lose! ${computerSelection} beats ${playerSelection}`
+    d += 1
+    console.log(`You Lose! ${computerSelection} beats ${playerSelection}`)
+    score.textContent = "Com's score: " + d
+    div.appendChild(score)
+    // return `You Lose! ${computerSelection} beats ${playerSelection}`
+         if (d === 5) {
+            score.textContent = 'You Lose!!'
+            div.appendChild(score)
+    }
     }
 }
 
@@ -73,12 +106,30 @@ function playGame() {
     }
 }
 
-playGame()
+const btnRock = document.createElement("button")
+const btnScissors = document.createElement("button")
+const btnPaper = document.createElement("button")
+const div = document.createElement("div")
+const body = document.querySelector("body")
+btnRock.textContent = "Rock"
+btnScissors.textContent = "Scissors"
+btnPaper.textContent = "Paper"
 
-if (c>0) {
-    alert("YOU WIN!")
-} else if (c=0) {
-    alert("TIE!")
-} else {
-    alert("YOU LOSE!")
-}
+body.appendChild(btnRock)
+body.appendChild(btnScissors)
+body.appendChild(btnPaper)
+body.appendChild(div)
+
+btnRock.addEventListener("click", () => playRound("rock", ))
+btnScissors.addEventListener("click", () => playRound("scissors", ))
+btnPaper.addEventListener("click", () => playRound("paper", ))
+
+// playGame()
+
+// if (c>0) {
+//     alert("YOU WIN!")
+// } else if (c=0) {
+//     alert("TIE!")
+// } else {
+//     alert("YOU LOSE!")
+// }
